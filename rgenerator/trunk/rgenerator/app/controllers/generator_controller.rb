295904e00b9e4e852141df7ab3d55b7a97501d80
glenn.rempe <glenn.rempe@635ac39e-fd36-0410-bce9-01c9dc5a8ffe>
2007-09-12 06:02:37 +0000

    r1740@mac-mini:  glenn | 2007-09-11 23:01:13 -0700  major work to get fully ajaxified version of the grid generator front end functioning.  choose customize grid to see form.  enter values and they will be automagically submitted and a new page width will be calculated.  The custom values are stored in the users session and can then be used to populate the grid.rcss template.  No work on hte template or the downloading functionality has been done.  Those should be the next steps.  We should get all of that chain fully working before attempting to work on the typography section.  As part of this process I vastly simplified the views and am using more rails helpers.  Local javascript has been removed.  All page updates with scriptaculous are triggered by ajax and sent from the server side which eliminates JS maint. how fun.

--- rgenerator/trunk/rgenerator/app/controllers/generator_controller.rb
+++ rgenerator/trunk/rgenerator/app/controllers/generator_controller.rb
@@ -3,4 +3,64 @@ class GeneratorController < ApplicationController
   def index
   end
   
+  def set_grid_default
+    # if the session[:grid] is nil we should ignore any custom settings when we generate
+    session[:grid] = nil
+    session[:grid_number_of_columns] = nil
+    session[:grid_column_width] = nil
+    session[:grid_margin_width] = nil
+    
+    render :update do |page|
+      page["grid_status"].replace_html "&nbsp;|&nbsp;" + "default"
+      page["grid_status"].visual_effect(:highlight, :duration => 2)
+      page["step_grid"].visual_effect(:fade)
+    end
+  end
+  
+  def set_grid_custom
+    # if the session[:grid] is 'custom' we need to use the custom settings when generating
+    session[:grid] = 'custom'
+    session[:grid_number_of_columns] = CssGrid.number_of_columns
+    session[:grid_column_width] = CssGrid.column_width
+    session[:grid_margin_width] = CssGrid.margin_width
+    
+    render :update do |page|
+      page["grid_status"].replace_html "&nbsp;|&nbsp;" + "custom"
+      page["grid_status"].visual_effect(:highlight, :duration => 2)
+      page["step_grid_form"].replace_html :partial => "step_grid_form"
+      page["step_grid"].visual_effect(:appear)
+      page["step_grid_form"].visual_effect(:highlight, :duration => 2)
+    end
+  end
+  
+  def set_grid_remove
+    # if the session[:grid] is 'remove' we need to ignore custom settings and not even include the grid when generating
+    session[:grid] = 'remove'
+    session[:grid_number_of_columns] = nil
+    session[:grid_column_width] = nil
+    session[:grid_margin_width] = nil
+    
+    render :update do |page|
+      page["grid_status"].replace_html "&nbsp;|&nbsp;" + "removed"
+      page["grid_status"].visual_effect(:highlight, :duration => 2)
+      page["step_grid"].visual_effect(:fade)
+    end
+  end
+  
+  def save_grid_custom_params
+    
+    # save custom params to the session as they come in:
+    session[:grid_number_of_columns] = params[:number_of_columns] unless params[:number_of_columns].nil?
+    session[:grid_column_width] = params[:column_width] unless params[:column_width].nil?
+    session[:grid_margin_width] = params[:margin_width] unless params[:margin_width].nil?
+    
+    # calculate the new page width
+    page_width = Grid.calc_page_width(session[:grid_column_width].to_i, session[:grid_margin_width].to_i, session[:grid_number_of_columns].to_i )
+    
+    render :update do |page|
+      page["grid_page_width"].replace_html page_width.to_s
+    end
+    
+  end
+  
 end
